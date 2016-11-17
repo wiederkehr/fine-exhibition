2d6ff785277e357d8c39ac2b5aebf8a985c08909
Benjamin Wiederkehr <benjamin@interactivethings.com>
2016-11-17 09:40:17 +0100

    Moved BackLink documentation into Header documentation.

--- src/docs/HeaderDoc.js
+++ src/docs/HeaderDoc.js
@@ -2,14 +2,20 @@ import React from 'react';
 import { Page, ReactSpecimen } from 'catalog';
 
 import Header from '../components/Header';
+import BackLink from '../components/BackLink';
 
 export default () => (
   <Page>
+    <h2>Header Variants</h2>
     <ReactSpecimen>
       <Header back={false} title='Title'/>
     </ReactSpecimen>
     <ReactSpecimen>
       <Header back={true} title='Title With Button'/>
+    </ReactSpecimen>
+    <h2>Header Actions</h2>
+    <ReactSpecimen>
+      <BackLink to="/backlink"/>
     </ReactSpecimen>
   </Page>
 );
