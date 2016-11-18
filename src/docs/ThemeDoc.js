9b3759e490b1b32c31c18bb142ed473de1b31e6c
Benjamin Wiederkehr <benjamin@interactivethings.com>
2016-11-18 09:35:02 +0100

    Added local font assets.

--- src/docs/ThemeDoc.js
+++ src/docs/ThemeDoc.js
@@ -25,7 +25,10 @@ export default () => (
             }
           />
         <h2>Fonts</h2>
-        <p>Here we will document the typography.</p>
+        <h3>Roboto Mono Regular: Body content</h3>
+        <p style={{fontFamily: 'Roboto Mono Regular'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <h3>Roboto Mono Medium: Headlines and interactive elements</h3>
+        <p style={{fontFamily: 'Roboto Mono Medium'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         <h2>Icons</h2>
         <p>Here we will document the iconography.</p>
       </Page>
