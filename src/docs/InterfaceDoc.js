e259f92e6dbd5c251212dba21bcfdad1963f4bbf
Benjamin Wiederkehr <benjamin@interactivethings.com>
2016-11-18 09:04:02 +0100

    Fixed type error.

--- src/docs/InterfaceDoc.js
+++ src/docs/InterfaceDoc.js
@@ -10,22 +10,22 @@ import BackLink from '../components/BackLink';
 export default () => (
       <Page>
         <h2>Header</h2>
-        <ReactSpecimen  noSource={true} span='3'>
+        <ReactSpecimen  noSource={true} span={3}>
           <Header back={false} title='Title'/>
         </ReactSpecimen>
-        <ReactSpecimen  noSource={true} span='3'>
+        <ReactSpecimen  noSource={true} span={3}>
           <Header back={true} title='Title With Button'/>
         </ReactSpecimen>
         <h3>Header Actions</h3>
-        <ReactSpecimen  noSource={true} span='3'>
+        <ReactSpecimen  noSource={true} span={3}>
           <BackLink to="/backlink"/>
         </ReactSpecimen>
         <h2>Button</h2>
-        <ReactSpecimen  noSource={true} span='3'>
+        <ReactSpecimen  noSource={true} span={3}>
           <Button type="button" label='Button' />
         </ReactSpecimen>
         <h2>Slider</h2>
-        <ReactSpecimen  noSource={true} span='3' state={{value: 3}}>
+        <ReactSpecimen  noSource={true} span={3} state={{value: 3}}>
           <Slider name="Slider"
                   value="3"
                   onChange={() => {}} />
