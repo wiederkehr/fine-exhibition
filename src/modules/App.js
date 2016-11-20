c694be9a90ca367471c4301545d12da502b3bcd3
Benjamin Wiederkehr <benjamin@interactivethings.com>
2016-11-20 14:42:23 +0100

    Moved the user variables for the database into the initial state which should become the store.

--- src/modules/App.js
+++ src/modules/App.js
@@ -3,6 +3,22 @@ import { Layout } from '../components/Layout';
 
 class App extends Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state={
+      user: {
+        name: 'Anna',
+        bookId: '5820e4e29f61d803006b2093',
+        sheetId: 'sheet',
+        baseUrl: 'https://api.fieldbook.com/v1/',
+        username: process.env.REACT_APP_FIELDBOOK_USER,
+        password: process.env.REACT_APP_FIELDBOOK_KEY,
+      }
+    }
+
+  };
+
   render() {
     return (
       <Layout>
