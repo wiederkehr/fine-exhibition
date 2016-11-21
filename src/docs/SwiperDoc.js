e9e90c461bae6036e8396d802539a94ad8ed0674
Benjamin Wiederkehr <benjamin@interactivethings.com>
2016-11-21 21:41:15 +0100

    Refactored the Swiper docs.

--- src/docs/SwiperDoc.js
+++ src/docs/SwiperDoc.js
@@ -2,17 +2,31 @@ import React from 'react';
 import { Page, ReactSpecimen } from 'catalog';
 import Swiper from '../components/Swiper';
 
-import './SwiperDoc.css';
+const SwiperViewport = {
+  height: '400px',
+  paddingBottom: '30px',
+  width: '650px',
+  overflow: 'auto'
+}
+const SwiperBlock1 = {
+  background: '#95ACCB',
+  height: '400px',
+  width: '400px'
+}
+const SwiperBlock2 = {
+  background: '#546D8E',
+  height: '400px',
+  width: '400px'
+}
 
 export default () => (
   <Page>
     <h2>Swiper</h2>
     <ReactSpecimen frame={true} noSource={true}>
-      <div className="SwiperViewport">
+      <div style={SwiperViewport}>
         <Swiper>
-          <div className="SwiperBlock SwiperBlock-1">Swiper Block #1</div>
-          <div className="SwiperBlock SwiperBlock-2">Swiper Block #2</div>
-          <div className="SwiperBlock SwiperBlock-3">Swiper Block #3</div>
+          <div style={SwiperBlock1}>Swiper Block #1</div>
+          <div style={SwiperBlock2}>Swiper Block #2</div>
         </Swiper>
       </div>
     </ReactSpecimen>
