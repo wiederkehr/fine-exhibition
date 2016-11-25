6d6d1c77976e010522307a896090de57a00a7d0e
Benjamin Wiederkehr <benjamin@interactivethings.com>
2016-11-25 09:44:56 +0100

    Responsive Swiper.

--- src/docs/SwiperDoc.js
+++ src/docs/SwiperDoc.js
@@ -2,33 +2,23 @@ import React from 'react';
 import { Page, ReactSpecimen } from 'catalog';
 import { Swiper } from '../components/Swiper';
 
-const SwiperViewport = {
-  height: '400px',
-  paddingBottom: '30px',
-  width: '650px',
-  overflow: 'auto'
-}
 const SwiperBlock1 = {
   background: '#95ACCB',
-  height: '400px',
-  width: '400px'
+  height: '610px',
 }
 const SwiperBlock2 = {
   background: '#546D8E',
-  height: '400px',
-  width: '400px'
+  height: '610px',
 }
 
 export default () => (
   <Page>
     <h2>Swiper</h2>
-    <ReactSpecimen frame={true} noSource={true}>
-      <div style={SwiperViewport}>
-        <Swiper>
-          <div style={SwiperBlock1}>Swiper Block #1</div>
-          <div style={SwiperBlock2}>Swiper Block #2</div>
-        </Swiper>
-      </div>
+    <ReactSpecimen noSource={true} span={3}>
+      <Swiper>
+        <div style={SwiperBlock1}>Swiper Block #1</div>
+        <div style={SwiperBlock2}>Swiper Block #2</div>
+      </Swiper>
     </ReactSpecimen>
   </Page>
 );
