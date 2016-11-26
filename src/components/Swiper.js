864e5f0e352c5721a0541f8da450c69d4940f1c2
Benjamin Wiederkehr <benjamin@interactivethings.com>
2016-11-26 16:15:09 +0100

    Rewrote the Swiper as a class.

--- src/components/Swiper.js
+++ src/components/Swiper.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
@@ -7,18 +7,28 @@ import './Swiper.css';
 // Documentation slick-carousel: https://github.com/kenwheeler/slick/
 // Documentation react-slick: https://github.com/akiran/react-slick
 
-export const Swiper = ({ children }) => {
+// Fix the loading width=0 with this function in ComponentDidMount: slick('setPosition');
 
-  var settings = {
-    arrows: false,
-    dots: true,
-    infinite: false,
-    mobileFirst: true,
-  };
+export class Swiper extends Component {
 
-  return (
-    <div className='Swiper'>
-      <Slider {...settings}>{children}</Slider>
-    </div>
-  )
-};
+  componentDidMount() {
+    console.log('resize');
+    // this.refs.slider.slickGoTo(e.target.value)
+    // $('.your-element').slick('setPosition');
+  }
+
+  render() {
+    const settings = {
+      arrows: false,
+      dots: true,
+      infinite: false,
+      mobileFirst: true,
+    };
+
+    return (
+      <div className='Swiper' ref="buttonContainer">
+        <Slider ref='swiper' {...settings}>{this.props.children}</Slider>
+      </div>
+    )
+  }
+}
