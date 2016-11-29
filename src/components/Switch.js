bfa89d52211088ea8eda21601235dd020c7acecf
Benjamin Wiederkehr <benjamin@interactivethings.com>
2016-11-29 09:29:33 +0100

    Added className prop.

--- src/components/Switch.js
+++ src/components/Switch.js
@@ -2,8 +2,8 @@ import React from 'react';
 import classNames from 'classnames';
 import './Switch.css';
 
-export const Switch = ({ state, onChange }) => (
-  <div className={ classNames('Switch', state) }>
+export const Switch = ({ className, state='on', onChange }) => (
+  <div className={ classNames('Switch', state, className) }>
     <button className={ classNames('SwitchButton', state) } type='button' onChange={onChange}>
       {state}
     </button>
