173383b332fd74e3c618ffeb9955bd35d60c314f
Benjamin Wiederkehr <benjamin@interactivethings.com>
2016-11-29 09:52:56 +0100

    Added settings page.

--- src/pages/SettingsPage.js
+++ src/pages/SettingsPage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { LayoutContainer, LayoutContent } from '../components/Layout';
 import { Header } from '../components/Header';
 import { Footer } from '../components/Footer';
+import { ListGroup, ListHeader, List, ListItem, ListLinkItem, ListSwitchItem, ListLinkSwitchItem } from '../components/List';
 
 import './SettingsPage.css';
 
@@ -12,7 +13,24 @@ class SettingsPage extends Component {
       <LayoutContainer>
         <Header left={{to:'/profile', label:'Back'}}>{this.props.route.title}</Header>
         <LayoutContent className="SettingsPageContent">
-          <p>Here will be the user settings.</p>
+          <ListGroup>
+            <ListHeader>Interface</ListHeader>
+            <List>
+              <ListSwitchItem state='on'>Diamonds</ListSwitchItem>
+              <ListSwitchItem state='off'>Clouds</ListSwitchItem>
+              <ListSwitchItem state='off'>Circles</ListSwitchItem>
+            </List>
+            <ListHeader>Notifications</ListHeader>
+            <List>
+              <ListLinkSwitchItem to='settings/notifications' state='on'>Scheduled Notifications</ListLinkSwitchItem>
+            </List>
+            <ListHeader>References</ListHeader>
+            <List>
+              <ListLinkItem to='settings/about'>About this application</ListLinkItem>
+              <ListLinkItem to='settings/disclaimer'>Disclaimer</ListLinkItem>
+              <ListLinkItem to='settings/imprint'>Imprint</ListLinkItem>
+            </List>
+          </ListGroup>
         </LayoutContent>
         <Footer/>
       </LayoutContainer>
