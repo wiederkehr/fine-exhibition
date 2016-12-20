0ac5698855accc6cddb0de555b82846768ae273b
Benjamin Wiederkehr <benjamin@interactivethings.com>
2016-12-20 18:26:51 +0100

    Refactored Recorder.

--- src/components/Recorder.js
+++ src/components/Recorder.js
@@ -1,37 +1,63 @@
 import React, { Component } from 'react';
+import { ActionForm, EmotionForm, EventForm, DimensionsForm, TriggerForm } from '../components/Form';
 import './Recorder.css';
 
 export class Recorder extends Component {
   render() {
     switch (this.props.step) {
-      case 'Emotion':
+      case 0:
         return (
-          <div className='Recorder'>Emotion</div>
+          <div className='Recorder'>
+            <EmotionForm
+              record={this.props.record}
+              onChange={this.props.onChange}
+            />
+          </div>
         )
-        break;
-      case 'Dimensions':
+      case 1:
         return (
-          <div className='Recorder'>Dimensions</div>
+          <div className='Recorder'>
+            <EventForm
+              record={this.props.record}
+              onChange={this.props.onChange}
+            />
+          </div>
         )
-        break;
-      case 'Triggers':
+      case 2:
         return (
-          <div className='Recorder'>Triggers</div>
+          <div className='Recorder'>
+            <DimensionsForm
+              record={this.props.record}
+              onChange={this.props.onChange}
+            />
+          </div>
         )
-        break;
-      case 'Actions':
+      case 3:
         return (
-          <div className='Recorder'>Actions</div>
+          <div className='Recorder'>
+            <TriggerForm
+              record={this.props.record}
+              onChange={this.props.onChange}
+            />
+          </div>
         )
-        break;
-      case 'Review':
+      case 4:
+        return (
+          <div className='Recorder'>
+            <ActionForm
+              record={this.props.record}
+              onChange={this.props.onChange}
+            />
+          </div>
+        )
+      case 5:
         return (
           <div className='Recorder'>Review</div>
         )
-        break;
+      default:
+        return (
+          <div className='Recorder'>Default</div>
+        )
     }
-    return (
-
-    )
   }
 }
