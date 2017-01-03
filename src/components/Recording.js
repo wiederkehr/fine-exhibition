d6e9a3d79c125a872e0bf56dc348e46df9bfdeb5
Benjamin Wiederkehr <benjamin@interactivethings.com>
2017-01-02 20:50:09 -0600

    Applied first dimension to Recording.

--- src/components/Recording.js
+++ src/components/Recording.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import classNames from 'classnames';
+import * as d3 from 'd3';
 import './Recording.css';
+
+const min       = 1;
+const max       = 5;
+const color     = d3.scaleLinear().domain([min, max]).range(["#ca0020", "#0571b0"]);
 
 export class Recording extends Component {
   render() {
     const classes = classNames('Recording', 'Recording--Step-' + this.props.step);
     return (
       <div className={classes}>
-        <Sky />
+        <Sky valence={this.props.record.valence}/>
         <Hummock />
         <Bummock />
         <Aura />
@@ -21,7 +26,7 @@ export class Recording extends Component {
 export class Sky extends Component {
   render() {
     return (
-      <div className='Sky'>Sky</div>
+      <div className='Sky' style={{ background: color(this.props.valence) }}>Sky</div>
     )
   }
 }
