e3f1b7910edbf7f0965bfc2db9221f6be116376d
Benjamin Wiederkehr <benjamin@interactivethings.com>
2017-01-15 18:27:50 +0100

    Extended button component to include primary and secondary.

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import './Button.css';
 
-export const Button = ({ disabled, onChange, type, children }) => (
-  <button className='Button' disabled={disabled} type={type} onChange={onChange}>
+export const Button = ({ disabled, onChange, type, style, children }) => (
+  <button
+    className='Button PrimaryButton'
+    disabled={disabled}
+    type={type}
+    onChange={onChange}
+    style={style}>
+    {children}
+  </button>
+);
+
+export const SecondaryButton = ({ disabled, onChange, type, style, children }) => (
+  <button
+    className='Button SecondaryButton'
+    disabled={disabled}
+    type={type}
+    onChange={onChange}
+    style={style}>
     {children}
   </button>
 );
