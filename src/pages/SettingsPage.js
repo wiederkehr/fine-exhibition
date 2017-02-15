f7e8b79c1be2c84011632d15ad562e1b2b4df111
Benjamin Wiederkehr <benjamin@interactivethings.com>
2017-02-15 22:49:16 +0100

    Cleaned up Settings pages.

--- src/pages/SettingsPage.js
+++ src/pages/SettingsPage.js
@@ -2,22 +2,11 @@ import React, { Component } from 'react';
 import { LayoutContainer, LayoutContent } from '../components/Layout';
 import { Header } from '../components/Header';
 import { Footer } from '../components/Footer';
-import { ListGroup, ListHeader, List, ListLinkItem, ListSwitchItem, ListLinkSwitchItem } from '../components/List';
+import { ListGroup, ListHeader, List, ListLinkItem, ListLinkSwitchItem } from '../components/List';
 
 import './SettingsPage.css';
 
 class SettingsPage extends Component {
-
-  constructor(props) {
-    super(props)
-
-    this.state={
-      diamonds: false,
-      clouds: false,
-      circles: true,
-    }
-
-  }
 
   render() {
     return (
@@ -25,18 +14,6 @@ class SettingsPage extends Component {
           <Header left={{to:'/profile', label:'Back'}}>{this.props.route.title}</Header>
           <LayoutContent className="SettingsPageContent">
             <ListGroup>
-              <ListHeader>Interface</ListHeader>
-              <List>
-                <ListSwitchItem
-                  status={this.state.diamonds}
-                  name='diamonds'>Diamonds</ListSwitchItem>
-                <ListSwitchItem
-                  status={this.state.clouds}
-                  name='clouds'>Clouds</ListSwitchItem>
-                <ListSwitchItem
-                  status={this.state.circles}
-                  name='circles'>Circles</ListSwitchItem>
-              </List>
               <ListHeader>Notifications</ListHeader>
               <List>
                 <ListLinkSwitchItem to='settings/notifications' status='on'>Scheduled Notifications</ListLinkSwitchItem>
@@ -46,6 +23,11 @@ class SettingsPage extends Component {
                 <ListLinkItem to='settings/about'>About</ListLinkItem>
                 <ListLinkItem to='settings/disclaimer'>Disclaimer</ListLinkItem>
                 <ListLinkItem to='settings/imprint'>Imprint</ListLinkItem>
+              </List>
+              <ListHeader>Data</ListHeader>
+              <List>
+                <ListLinkItem to='settings/import'>Import</ListLinkItem>
+                <ListLinkItem to='settings/export'>Export</ListLinkItem>
               </List>
             </ListGroup>
           </LayoutContent>
