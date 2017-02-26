d472bf6493bf0e48fad8b84d5e8fbca4697a8fcf
Benjamin Wiederkehr <benjamin@interactivethings.com>
2017-02-26 19:57:02 +0100

    Used the new EmotionHeader component in Recorder
    This might be dangerous as I will have to manually position the text. Let’s see.

--- src/components/Recorder.js
+++ src/components/Recorder.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactCSSTransitionGroup from 'react/lib/ReactCSSTransitionGroup';
+import { EmotionHeader } from './Detail.js';
 import { ActionForm, EmotionForm, DimensionsForm, TriggerForm } from '../components/Form';
 import './Recorder.css';
 
@@ -61,27 +62,10 @@ export class Recorder extends Component {
         return (
           <RecorderTransition direction={this.props.direction} step={this.props.step}>
             <div className='Recorder'>
-              <h2 className='RecorderHeadline'>{this.props.record.emotion}</h2>
-              <h3 className='RecorderSubheadline'>{this.props.record.date}</h3>
-              <table style={{marginBottom:'30px', width:'100%', padding: '10px', background: 'rgba(255,255,255,0.7)'}}>
-                <thead>
-                  <tr>
-                    <th style={{textAlign: 'left'}}>Dimension</th>
-                    <th style={{textAlign: 'right'}}>Value</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr><td>Emotion</td><td style={{textAlign: 'right'}}>{this.props.record.emotion}</td></tr>
-                  <tr><td>Date</td><td style={{textAlign: 'right'}}>{this.props.record.date}</td></tr>
-                  <tr><td>Trigger</td><td style={{textAlign: 'right'}}>{this.props.record.trigger}</td></tr>
-                  <tr><td>Action</td><td style={{textAlign: 'right'}}>{this.props.record.action}</td></tr>
-                  <tr><td>Arousal</td><td style={{textAlign: 'right'}}>{this.props.record.arousal}</td></tr>
-                  <tr><td>Conduciveness</td><td style={{textAlign: 'right'}}>{this.props.record.conduciveness}</td></tr>
-                  <tr><td>Controllability</td><td style={{textAlign: 'right'}}>{this.props.record.controllability}</td></tr>
-                  <tr><td>Intensity</td><td style={{textAlign: 'right'}}>{this.props.record.intensity}</td></tr>
-                  <tr><td>Valence</td><td style={{textAlign: 'right'}}>{this.props.record.valence}</td></tr>
-                </tbody>
-              </table>
+              <EmotionHeader
+                emotion={this.props.record.emotion}
+                date={this.props.record.date}
+              />
             </div>
           </RecorderTransition>
         )
