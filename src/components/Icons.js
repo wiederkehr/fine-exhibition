bc9569c182be063bf5c48401942927e2faf54a6b
Benjamin Wiederkehr <benjamin@interactivethings.com>
2017-06-01 09:41:48 +0200

    Consolidated the Icons.

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -1,5 +1,14 @@
 import React from 'react';
-import './DetailIcon.css';
+import './Icons.css';
+
+export const CloseIcon = () => (
+  <svg className='CloseIcon' width='16' height='16'>
+    <g>
+      <line x1="2" y1="2" x2="14" y2="14"/>
+      <line x1="2" y1="14" x2="14" y2="2"/>
+    </g>
+  </svg>
+);
 
 export const DetailIcon = () => (
   <svg className='DetailIcon' width='24' height='24'>
