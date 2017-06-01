baf10946b3652498af13fef377600a6aab75b383
Benjamin Wiederkehr <benjamin@interactivethings.com>
2017-06-01 14:52:18 +0200

    Semi-successfully implemented React Motion.

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import Transition from 'react-motion-ui-pack';
+import { spring } from 'react-motion';
 import './Notification.css';
 
 export const Notification = ( props ) => {
-  setTimeout(props.clear, 3000)
+  setTimeout(props.clear, 3000);
   return (
-    <div className='Notification'>
-      <span className='NotificationMessage'>
-        { props.children }
-      </span>
-    </div>
-  );
+    <Transition
+      component={false}
+      enter={{
+        opacity: 1,
+        translateY: spring(0, {stiffness: 200, damping: 10})
+      }}
+      leave={{
+        opacity: 0,
+        translateY: -50
+      }}
+      >
+        <div className='NotificationWrapper'>
+          <div className='Notification'>
+            <span className='NotificationMessage'>
+              { props.children }
+            </span>
+          </div>
+        </div>
+      </Transition>
+    );
 }
