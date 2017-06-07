2d471704cdeaacc89d8a06c51ab95b90ba9b03b2
Benjamin Wiederkehr <benjamin@interactivethings.com>
2017-06-07 22:47:18 +0200

    Refined vertical spacing for dimension sliders.

--- src/components/RecorderDimension.js
+++ src/components/RecorderDimension.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { DimensionSlider } from '../components/Slider';
 import { Dimensions } from '../content/Options';
+import { LayoutRow } from '../components/Layout';
 
 export const DimensionRecorder = ( props ) => {
   return (
     <div className='Recorder Recorder--Dimensions'>
-      <h2 className='RecorderHeadline'>How does this feel?</h2>
       <DimensionForm
         record={props.record}
         onChange={props.onChange}
@@ -18,31 +18,41 @@ export const DimensionRecorder = ( props ) => {
 const DimensionForm = ( props ) => (
   <form className='Form' onSubmit={props.onSubmit}>
     <div className='Slider-group'>
-      <DimensionSlider
-        name='How intense was it?'
-        value={props.record.intensity}
-        labels={Dimensions.intensity}
-        onChange={props.onChange.bind(null, 'intensity')} />
-      <DimensionSlider
-        name='How pleasant was it?'
-        value={props.record.valence}
-        labels={Dimensions.valence}
-        onChange={props.onChange.bind(null, 'valence')} />
-      <DimensionSlider
-        name='How controllable was it?'
-        value={props.record.controllability}
-        labels={Dimensions.controllability}
-        onChange={props.onChange.bind(null, 'controllability')} />
-      <DimensionSlider
-        name='How aroused was your body?'
-        value={props.record.arousal}
-        labels={Dimensions.arousal}
-        onChange={props.onChange.bind(null, 'arousal')} />
-      <DimensionSlider
-        name='How conducive is it?'
-        value={props.record.conduciveness}
-        labels={Dimensions.conduciveness}
-        onChange={props.onChange.bind(null, 'conduciveness')} />
+      <LayoutRow bottom='xl'>
+        <DimensionSlider
+          name='How intense was it?'
+          value={props.record.intensity}
+          labels={Dimensions.intensity}
+          onChange={props.onChange.bind(null, 'intensity')} />
+      </LayoutRow>
+      <LayoutRow top='xl' bottom='xl'>
+        <DimensionSlider
+          name='How pleasant was it?'
+          value={props.record.valence}
+          labels={Dimensions.valence}
+          onChange={props.onChange.bind(null, 'valence')} />
+      </LayoutRow>
+      <LayoutRow top='xl' bottom='xl'>
+        <DimensionSlider
+          name='How controllable was it?'
+          value={props.record.controllability}
+          labels={Dimensions.controllability}
+          onChange={props.onChange.bind(null, 'controllability')} />
+      </LayoutRow>
+      <LayoutRow top='xl' bottom='xl'>
+        <DimensionSlider
+          name='How aroused was your body?'
+          value={props.record.arousal}
+          labels={Dimensions.arousal}
+          onChange={props.onChange.bind(null, 'arousal')} />
+      </LayoutRow>
+      <LayoutRow top='xl' bottom='xl'>
+        <DimensionSlider
+          name='How conducive is it?'
+          value={props.record.conduciveness}
+          labels={Dimensions.conduciveness}
+          onChange={props.onChange.bind(null, 'conduciveness')} />
+      </LayoutRow>
     </div>
   </form>
 );
